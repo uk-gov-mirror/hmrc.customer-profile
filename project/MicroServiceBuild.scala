4ea42a305df322fe78397455b9d5a58688048a39
nickpace <pacenick@gmail.com>
2016-10-24 10:39:58 +0100

    NGC-850 - Update libs.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -20,15 +20,15 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val microserviceBootstrapVersion = "5.0.0"
-  private val playAuthVersion = "4.0.0"
+  private val microserviceBootstrapVersion = "5.1.0"
+  private val playAuthVersion = "4.1.0"
   private val playHealthVersion = "2.0.0"
   private val playJsonLoggerVersion = "2.1.1"
   private val playUrlBindersVersion = "2.0.0"
   private val playConfigVersion = "3.0.0"
-  private val domainVersion = "3.7.0"
+  private val domainVersion = "4.0.0"
   private val playHmrcApiVersion = "0.6.0"
-  private val hmrcTestVersion = "1.9.0"
+  private val hmrcTestVersion = "2.0.0"
   private val pegdownVersion = "1.6.0"
   private val scalaTestVersion = "2.2.6"
   private val wireMockVersion = "1.58"
