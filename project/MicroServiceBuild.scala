7a3c66f44600c7deaa437bb2fe200a005cda8cad
nickpace <pacenick@gmail.com>
2016-10-31 12:44:58 +0000

    NGC-755 - Update library for play-hmrc-api.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -25,7 +25,7 @@ private object AppDependencies {
   private val playUrlBindersVersion = "2.0.0"
   private val playConfigVersion = "3.0.0"
   private val domainVersion = "4.0.0"
-  private val playHmrcApiVersion = "1.1.0"
+  private val playHmrcApiVersion = "1.2.0"
   private val hmrcTestVersion = "2.0.0"
   private val pegdownVersion = "1.6.0"
   private val scalaTestVersion = "2.2.6"
