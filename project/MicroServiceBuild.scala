82e5ec57b58754921bf5daade0c3cf0bf0ea9fdf
nickpace <pacenick@gmail.com>
2016-11-07 09:13:19 +0000

    NGC-918 - Switch back to json-play-logger since MDC not initialised with appName.

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -40,7 +40,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
     "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
-    "uk.gov.hmrc" %% "logback-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
     "uk.gov.hmrc" %% "reactive-circuit-breaker" % "1.7.0",
     "uk.gov.hmrc" %% "emailaddress" % "1.1.0"
