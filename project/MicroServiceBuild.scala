159bf3ca73f5a92f72258fbd4fe093dbf09e1629
Dimitri Theulings <dimitri@tastypies.co.uk>
2016-11-22 16:40:55 +0000

    Upgrade dependencies to latest available versions

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -18,7 +18,7 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val microserviceBootstrapVersion = "5.6.0"
+  private val microserviceBootstrapVersion = "5.8.0"
   private val playAuthVersion = "4.2.0"
   private val playHealthVersion = "2.0.0"
   private val playJsonLoggerVersion = "3.0.0"
@@ -26,11 +26,13 @@ private object AppDependencies {
   private val playConfigVersion = "3.0.0"
   private val domainVersion = "4.0.0"
   private val playHmrcApiVersion = "1.2.0"
-  private val hmrcTestVersion = "2.0.0"
+  private val hmrcTestVersion = "2.2.0"
   private val pegdownVersion = "1.6.0"
   private val scalaTestVersion = "2.2.6"
   private val wireMockVersion = "2.2.2"
   private val cucumberVersion = "1.2.5"
+  private val reactiveCircuitBreaker = "2.0.0"
+  private val emailAddress = "2.0.0"
 
   val compile = Seq(
     ws,
@@ -42,8 +44,8 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
     "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
-    "uk.gov.hmrc" %% "reactive-circuit-breaker" % "1.7.0",
-    "uk.gov.hmrc" %% "emailaddress" % "1.1.0"
+    "uk.gov.hmrc" %% "reactive-circuit-breaker" % reactiveCircuitBreaker,
+    "uk.gov.hmrc" %% "emailaddress" % emailAddress
   )
 
   trait TestDependencies {
@@ -72,7 +74,7 @@ private object AppDependencies {
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
         "com.github.tomakehurst" % "wiremock" % wireMockVersion % scope,
-        "org.mockito" % "mockito-all" % "1.9.5" % scope,
+        "org.mockito" % "mockito-all" % "1.10.19" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % scope
       )
     }.test
