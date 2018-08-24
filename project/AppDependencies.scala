ae299cac4af12dca26497d7aae3cdd9a704d475e
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2018-08-24 09:11:32 +0100

    [NGC-3674][LS] Migrating to new build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,59 +1,59 @@
-import sbt._
-
-object AppDependencies {
-  import play.core.PlayVersion
-  import play.sbt.PlayImport._
-
-  private val bootstrap25Version = "1.7.0"
-  private val authClientVersion = "2.6.0"
-  private val domainVersion = "5.2.0"
-  private val playHmrcApiVersion = "2.1.0"
-  private val reactiveCircuitBreakerVersion = "3.2.0"
-  private val emailaddressVersion = "2.2.0"
-
-  val compile = Seq(
-    ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrap25Version,
-    "uk.gov.hmrc" %% "auth-client" % authClientVersion,
-    "uk.gov.hmrc" %% "play-hmrc-api" % playHmrcApiVersion,
-    "uk.gov.hmrc" %% "domain" % domainVersion,
-    "uk.gov.hmrc" %% "reactive-circuit-breaker" % reactiveCircuitBreakerVersion,
-    "uk.gov.hmrc" %% "emailaddress" % emailaddressVersion
-  )
-
-  trait TestDependencies {
-    lazy val scope: String = "test"
-    lazy val test : Seq[ModuleID] = ???
-  }
-
-  private val hmrcTestVersion = "3.0.0"
-  private val scalatestplusPlayVersion = "2.0.1"
-  private val wiremockVersion = "2.9.0"
-  private val scalamockVersion = "4.0.0"
-
-  object Test {
-    def apply(): Seq[ModuleID] = new TestDependencies {
-      override lazy val test = Seq(
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        "org.scalamock" %% "scalamock" % scalamockVersion % scope
-      )
-    }.test
-  }
-
-  object IntegrationTest {
-    def apply(): Seq[ModuleID] = new TestDependencies {
-
-      override lazy val scope: String = "it"
-
-      override lazy val test = Seq(
-        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
-        "com.github.tomakehurst" % "wiremock" % wiremockVersion % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusPlayVersion % scope
-      )
-    }.test
-  }
-
-  def apply(): Seq[ModuleID] = compile ++ Test() ++ IntegrationTest()
-}
+//import sbt._
+//
+//object AppDependencies {
+//  import play.core.PlayVersion
+//  import play.sbt.PlayImport._
+//
+//  private val bootstrap25Version = "1.7.0"
+//  private val authClientVersion = "2.6.0"
+//  private val domainVersion = "5.2.0"
+//  private val playHmrcApiVersion = "2.1.0"
+//  private val reactiveCircuitBreakerVersion = "3.2.0"
+//  private val emailaddressVersion = "2.2.0"
+//
+//  val compile = Seq(
+//    ws,
+//    "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrap25Version,
+//    "uk.gov.hmrc" %% "auth-client" % authClientVersion,
+//    "uk.gov.hmrc" %% "play-hmrc-api" % playHmrcApiVersion,
+//    "uk.gov.hmrc" %% "domain" % domainVersion,
+//    "uk.gov.hmrc" %% "reactive-circuit-breaker" % reactiveCircuitBreakerVersion,
+//    "uk.gov.hmrc" %% "emailaddress" % emailaddressVersion
+//  )
+//
+//  trait TestDependencies {
+//    lazy val scope: String = "test"
+//    lazy val test : Seq[ModuleID] = ???
+//  }
+//
+//  private val hmrcTestVersion = "3.0.0"
+//  private val scalatestplusPlayVersion = "2.0.1"
+//  private val wiremockVersion = "2.9.0"
+//  private val scalamockVersion = "4.0.0"
+//
+//  object Test {
+//    def apply(): Seq[ModuleID] = new TestDependencies {
+//      override lazy val test = Seq(
+//        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+//        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+//        "org.scalamock" %% "scalamock" % scalamockVersion % scope
+//      )
+//    }.test
+//  }
+//
+//  object IntegrationTest {
+//    def apply(): Seq[ModuleID] = new TestDependencies {
+//
+//      override lazy val scope: String = "it"
+//
+//      override lazy val test = Seq(
+//        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+//        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+//        "com.github.tomakehurst" % "wiremock" % wiremockVersion % scope,
+//        "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusPlayVersion % scope
+//      )
+//    }.test
+//  }
+//
+//  def apply(): Seq[ModuleID] = compile ++ Test() ++ IntegrationTest()
+//}
